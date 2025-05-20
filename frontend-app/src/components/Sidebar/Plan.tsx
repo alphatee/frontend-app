c14c329fae96cbcfe19a0b4a9eef3b433ef7735d
Daniel Herrera <dher426@wgu.edu>
2025-05-20 04:35:48 -0700

    Finished App, ready to publish

--- frontend-app/src/components/Sidebar/Plan.tsx
+++ frontend-app/src/components/Sidebar/Plan.tsx
@@ -5,7 +5,7 @@ export const Plan = () => {
     <div className="flex sticky top-[calc(100vh_-_48px_-_16px)] flex-col h-12 border-t px-2 border-stone-300 justify-end text-xs">
       <div className="flex items-center justify-between">
         <div>
-          <p className="font-bold">Enterprise</p>
+          <p className="font-bold text-violet-500">Enterprise</p>
           <p className="text-stone-500">Pay as you go</p>
         </div>
 
--- frontend-app/src/components/Sidebar/Sidebar.tsx
+++ frontend-app/src/components/Sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ import { Plan } from "./Plan";
 export const Sidebar = () => {
   return (
     <div>
-      <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+      <div className="sticky top-4 h-[calc(100vh-32px-48px)]">
         <AccountToggle />
         <Search />
         <RouteSelect />
