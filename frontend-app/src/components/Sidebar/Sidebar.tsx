ec657cd50a067088559ef7491e82a8cc919e85cc
Daniel Herrera <dher426@wgu.edu>
2025-05-20 03:10:38 -0700

    finished Sidebar

--- frontend-app/src/components/Sidebar/Sidebar.tsx
+++ frontend-app/src/components/Sidebar/Sidebar.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { AccountToggle } from './AccountToggle'
+import React from "react";
+import { AccountToggle } from "./AccountToggle";
+import { Search } from "./Search";
+import { RouteSelect } from "./RouteSelect";
+import { Plan } from "./Plan";
 
 export const Sidebar = () => {
   return (
-  <div>
-    <div className="overflow-y-scroll
+    <div>
+      <div
+        className="overflow-y-scroll
      sticky top-4 h-[calc
-     (100vh-32px-48px)]">
+     (100vh-32px-48px)]"
+      >
         <AccountToggle />
-  </div>
+        <Search />
+        <RouteSelect />
+      </div>
 
-        {/*TODO: Plan toggle */}
-  </div>
+      {/*TODO: Plan toggle */}
+      <Plan />
+    </div>
   );
 };
